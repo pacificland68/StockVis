8005cbbcd22f080dce0d6c4898b214f69f40ff8e
Jack Aubrey <pacificland68@hotmail.com>
2024-02-26 23:30:23 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),{
+    name: 'vite-plugin-threejs',
+    options: {
+      // 配置 Three.js 的路径
+      path: 'node_modules/three/build/three.module.js'
+    }
+  }],
   server: {
     host: '0.0.0.0'
   }
